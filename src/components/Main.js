6e8cb53f6b040c77d65397fbf5b4a169569f9fbc
Daniel Hill <daniel.s.hill01@gmail.com>
2021-10-28 12:57:16 -0600

    removed animal and human branches

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,8 @@
 import { useState, useEffect } from 'react';
 import { findRenderedComponentWithType } from 'react-dom/test-utils';
 import { Route, Switch, Redirect, Link } from 'react-router-dom';
-import AnimalIndex from '../pages/AnimalIndex'
-import AnimalShow from '../pages/AnimalShow'
 import EnemyIndex from '../pages/EnemyIndex';
 import EnemyShow from '../pages/EnemyShow';
-import HumanIndex from '../pages/HumanIndex';
-import HumanShow from '../pages/HumanShow';
 
 
 
